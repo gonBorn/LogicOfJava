8b4d32483f7758354b34cda355b66fa931472498
duzeyan <gonBorn@163.com>
2021-01-20 18:44:40 +0800

    [duzeyan] feat: 迭代器模式

---
+++ src/main/java/designPattern/IteratorPattern.java
@@ -0,0 +1,15 @@
+package designPattern;
+
+import java.util.Iterator;
+
+public class IteratorPattern implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+}
